5576a168e892973eaf285197c51cb20558f7696a
Christian Thompson <ChristianThompson1@hotmail.com>
2021-12-08 19:14:27 -0800

    added screen to show notes on home screen

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
-# from datetime import timedelta
+import datetime 
 
 
 app = Flask(__name__)
@@ -17,11 +17,47 @@ app.secret_key = "bdqsrmf2"
 # app.permanent_session_lifetime = timedelta(minutes=30)
 
 
- # -------- Home Page
+#------------------------------------------------------------
+
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', notes=notes.find())
+
+
+# Form to add new note and fill out 
+@app.route('/notes/new')
+def notes_new():
+    return render_template('notes_new.html', title='New Note')
+
+
+# Submitting the note to the database
+@app.route('/notes', methods=['POST'])
+def notes_submit():
+    note = {
+        'name': request.form.get('dname'),
+        'content': request.form.get('desc'),
+        'created': datetime.datetime.utcnow(),
+    }
+    notes.insert_one(note)
+    return redirect(url_for('home'))
+
+@app.route('/notes/<note_id>')
+def notes_show(note_id):
+    note = notes.find_one({'_id': ObjectId(note_id)})
+    return render_template('index.html', note=note)
+
+
+# Edit a note
+@app.route('/notes/<notes_id>/edit')
+
+
+# # Update a note
+# @app.route('/notes/:id')
+
+
+@app.route('/notes/:id/delete')
+def delete_note():
 
 # USER INFO --------------------------------------------------
 
@@ -37,7 +73,7 @@ def login():
         return redirect(url_for("user"))
     else:
         if "user" in session:
-            flash("Already logged in!!")
+            # flash("Already logged in!!")
             return redirect(url_for("user"))
 
         return render_template('login.html')
@@ -49,7 +85,7 @@ def user():
         return render_template("user.html", user=user)
     else:
         flash("You are not logged in!")
-        return redirect(url_for("login"))
+        return redirect(url_for("login"), notes=notes.find())
 
 @app.route("/logout")
 def logout():
@@ -57,40 +93,6 @@ def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
 
-#------------------------------------------------------------
-
-# Create and submit a note
-@app.route('/notes')
-def notes_index():
-    return render_template('index.html')
-
-
-# Form to add new note and fill out 
-@app.route('/notes/new')
-def notes_new():
-    return render_template('notes_new.html', title='New Note')
-
-
-@app.route('/notes', methods=['POST'])
-def notes_submit():
-    note = {
-        'note_name': request.form.get('dname'),
-        'note_content': request.form.get('desc'),
-        'date': request.form.get('date'),
-    }
-    notes.insert_one(note)
-    return redirect(url_for('home'))
-
-
-# # Edit a note
-# @app.route('/notes/<notes_id>/edit')
-
-
-# # Update a note
-# @app.route('/notes/:id')
-
-
-# @app.route('/notes/:id/delete')
 
 if __name__ == "__main__":
     app.run(debug=True)
